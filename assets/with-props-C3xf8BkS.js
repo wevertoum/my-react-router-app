cdbcdb83a40a549915c690cae949c94c8022754d
wevertoum <wevertonmixalt@gmail.com>
2025-04-10 10:27:44 -0300

    Updates

---
+++ assets/with-props-C3xf8BkS.js
@@ -0,0 +1,1 @@
+import{p,q as t,t as e,v as o,r as s,w as u}from"./chunk-KNED5TY2-Ci3HgUYM.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
